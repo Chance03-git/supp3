20af5b7ab00f67c93d4ca541fe2c27b68b78ea7b
Chance03-git <Chancefoster05@gmail.com>
2024-11-22 18:25:50 -0500

    function forfahreneheit to celcius

--- temp.py
+++ temp.py
@@ -8,8 +8,8 @@ def celsius_to_fahrenheit(celsius):
         The temperature in Fahrenheit.
     """
     return (celsius * 9/5) + 32
-
-
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
 def test_should_celsius_to_fahreneheit():
     assert celsius_to_fahrenheit(100) == 212
 def test_should_return_fahreneheit_to_celsius():
