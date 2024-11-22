b7c9715c9c244b01251b18783cf1d2f2f3cad510
Chance03-git <Chancefoster05@gmail.com>
2024-11-22 18:15:13 -0500

    documentation for celcius to fahrenheit function

--- temp.py
+++ temp.py
@@ -1,4 +1,12 @@
 def celsius_to_fahrenheit(celsius):
+    """Converts a temperature from Celsius to Fahrenheit.
+
+    Args:
+        celsius: The temperature in Celsius.
+
+    Returns:
+        The temperature in Fahrenheit.
+    """
     return (celsius * 9/5) + 32
 
 def test_should_celsius_to_fareneheit():
